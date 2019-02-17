b38b4527a613f2efb119edd79af4791ed6d719be
Sergey <codename.cobweb@gmail.com>
2019-02-17 14:52:52 +0300

    fix

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const hbs = require('hbs');
+
+const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
+const dotenv = require('dotenv');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+dotenv.config({
+  path: path.join(__dirname, '.env'),
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose
+    .connect(
+        process.env.MONGO_URL, {
+          useNewUrlParser: true,
+        },
+    )
+    .then(() => {
+      console.log('Connection mongodb success!!!');
+    })
+    .catch((error) => console.log(error));
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'hbs');
+app.set('view options', {
+  layout: 'layouts/layout',
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
